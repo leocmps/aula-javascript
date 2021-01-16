eeb86b41e11e025f9001c3577d52cb05f2c0b7cb
leocmps <leocampos06@hotmail.com>
2021-01-16 11:41:28 -0300

    Aula JS :)

---
+++ js/script.js
@@ -0,0 +1,107 @@
+function obrigado(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por acessar meu site :D !"
+}
+
+function acessaSite(url){
+    window.open("https://www.linkedin.com/in/leonardo-campos-41b09b1a4/");
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Muito bem!"
+    //alert("Muito bem!")
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui"
+}
+/*
+function soma(n1, n2){
+    return n1 + n2
+}
+*/
+
+/*
+function validaIdade(idade){
+    var validar;
+    if(idade >= 18){
+        validar = true;
+    }else{
+        validar = false;
+    }
+    return validar;
+}
+*/
+
+/*
+var idade = prompt("Qual sua idade?")
+console.log(validaIdade(idade));
+*/
+
+//alert(soma(5,10));
+
+/*
+var d = new Date();
+alert(d.getMinutes());
+*/
+
+/*
+var count;
+for (count=0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 5;
+while (count < 5){
+    console.log(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt("Qual sua idade?");
+if (idade >= 18){  // > MAIOR QUE | >= MAIOR OU IGUAL
+    alert("maior de idade!")
+}else{
+    alert("menor de idade.")
+}
+*/
+
+/*
+var frutas = [{nome:"banana",cor:"amarela"}, {nome:"maçã", cor:"vermelha"}];
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta = {nome:"laranja", cor:"laranja"};
+console.log(fruta);
+console.log(fruta.nome);
+console.log(fruta.cor);
+*/
+
+/*
+var lista = ["maça", "pera", "laranja"];
+lista.push("banana");
+lista.pop();
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "));
+*/
+
+/*
+var n1 = 10
+var n2 = 3
+alert(n1 * n2);
+
+var nome = "Leonardo Campos";
+var idade = 18;
+alert(nome + " tem " + idade + " anos.");
+
+console.log(nome);
+console.log(idade);
+*/
+
